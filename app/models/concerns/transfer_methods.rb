298e7bd4bead9db199d82ffd0949e4ddec8be0d7
John Coote <john_coote@rocketmail.com>
2023-05-28 14:28:40 +1000

    concern

---
+++ app/models/concerns/transfer_methods.rb
@@ -0,0 +1,59 @@
+module TransferMethods
+  extend ActiveSupport::Concern
+
+  included do
+    def first_degree_given_transfers
+      given_transfers
+    end
+
+    def first_degree_received_transfers
+      received_transfers
+    end
+
+    def first_degree_given_transfer_nodes
+      # who did this person/group give to?
+      first_degree_given_transfers.map(&:taker)
+    end
+
+    def first_degree_received_transfer_nodes
+      # who did this person/group receive from?
+      first_degree_received_transfers.map(&:giver)
+    end
+
+    def second_degree_given_transfers
+      nodes.flat_map(&:given_transfers)
+    end
+
+    def second_degree_received_transfers
+      nodes.flat_map(&:received_transfers)
+    end
+
+    def second_degree_given_transfer_nodes
+      # of all the nodes who are connected to this group, what are the nodes they gave to?
+      second_degree_given_transfers.map(&:taker).flatten - [self]
+    end
+
+    def second_degree_received_transfer_nodes
+      # of all the nodes who are connected to this group, what are the nodes they received transfers from?
+      second_degree_received_transfers.map(&:giver).flatten - [self]
+    end
+
+    def third_degree_given_transfers
+      nodes.flat_map(&:second_degree_given_transfers)
+    end
+
+    def third_degree_received_transfers
+      nodes.flat_map(&:second_degree_received_transfers)
+    end
+
+    def third_degree_given_transfer_nodes
+      # of all the nodes who are connected to this group, what are the nodes they gave to?
+      third_degree_given_transfers.map(&:taker).flatten - nodes - [self]
+    end
+
+    def third_degree_received_transfer_nodes
+      # of all the nodes who are connected to this group, what are the nodes they received transfers from?
+      third_degree_received_transfers.map(&:giver).flatten - nodes - [self]
+    end
+  end
+end
