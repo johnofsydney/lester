505d0b3dfc2c67a9bc946db1e8a1aa61f9de5d07
John Coote <john.coote@sherpa.net.au>
2024-04-18 20:31:34 +1000

    cleaning csv files from aec

--- app/views/home/index_view.rb
+++ app/views/home/index_view.rb
@@ -27,7 +27,8 @@ class Home::IndexView < ApplicationView
 
       ul(style: 'list-style-type: none;') do
         li do
-          a(href: 'https://transparency.aec.gov.au/AnnualDonor') { 'AEC Annual Donor records for the last 5 years' }
+          a(href: 'https://transparency.aec.gov.au/AnnualDonor') { 'AEC Annual Donor records for the last 10 years' }
+          a(href: 'https://en.wikipedia.org/wiki/Category:Members_of_Australian_parliaments_by_term') { 'Federal MPs and Senators since 2016' }
         end
       end
 
