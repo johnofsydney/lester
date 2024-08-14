a3d68628be223e92ae7cc98d89429e76c85d5c3a
John Coote <john_coote@rocketmail.com>
2024-08-14 14:55:01 +1000

    tweak build queue a little

--- app/controllers/lazy_load_groups_controller.rb
+++ app/controllers/lazy_load_groups_controller.rb
@@ -1,5 +1,6 @@
 class LazyLoadGroupsController < ApplicationController
   def show
+    # this is the controller action defined by lazy_load_group_path and called in the turbo frame
     @group = Group.find(params[:id])
   end
 end
