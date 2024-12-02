c117ce977557e70618147a37875115c0fd9a7df7
John Coote <john_coote@rocketmail.com>
2024-12-02 23:13:08 +1100

    uses consolidated_transfers for nodes and edges

--- app/controllers/inertia_controller.rb
+++ app/controllers/inertia_controller.rb
@@ -12,18 +12,52 @@ class InertiaController < ApplicationController
   end
 
   def active_nodes
-    person_nodes + person
+    person_nodes # + person
   end
 
   def person_nodes
-    @person.nodes.map { |node| {id: node.id, label: node.name} }
+    @person.consolidated_descendents(depth: 2)
+           .map { |node| configure_node(node) }
   end
 
-  def person
-    [{id: -@person.id, label: @person.name}]
+  def ids_people_descendents
+    @person.consolidated_descendents(depth: 2)
+           .filter{|n| n.klass == 'Person' }
+           .map{|n| n.id }
+  end
+
+  def ids_group_descendents
+    @person.consolidated_descendents(depth: 2)
+           .filter{|n| n.klass == 'Group' }
+           .map{|n| n.id }
   end
 
   def person_edges
-    @person.nodes.map { |node| {from: -@person.id, to: node.id} }
+    # @person.nodes.map { |node| {from: -@person.id, to: node.id} }
+
+    Membership.where(member_id: ids_people_descendents, member_type: 'Person')
+              .or(Membership.where(member_id: ids_group_descendents, member_type: 'Group'))
+              .or(Membership.where(group_id: ids_group_descendents))
+              .map { |membership| configure_edge(membership) }
+  end
+
+  def configure_edge(membership)
+    member_id = membership.member_type == "Person" ? -membership.member_id : membership.member_id
+    {
+      from: member_id,
+      to: membership.group_id
+    }
+  end
+
+  def configure_node(node)
+    id = node.klass == "Person" ? -node.id : node.id
+
+    {
+      id: id,
+      label: node.name,
+    }
   end
 end
+
+
+# Membership.where(member_id: 875, member_type: 'Person').or(Membership.where(member_id: 228, member_type: 'Group')).or(Membership.where(group_id: 228))
--- app/javascript/pages/PersonNetworkGraph.jsx
+++ app/javascript/pages/PersonNetworkGraph.jsx
@@ -8,12 +8,19 @@ import Graph from 'react-vis-network-graph'
 // import viteRubySvg from '/assets/vite_ruby.svg'
 
 import cs from './InertiaExample.module.css'
+import { shape } from 'prop-types'
 
 export default function PersonNetworkGraph({ name, active_nodes, person_edges }) {
 
   const parsed_nodes = JSON.parse(active_nodes).map((node) => {
+    const color = node.label === name ? 'rgba(240,110,110' : 'rgba(220,220,115'
             return (
-              { id: node.id, label: node.label }
+              {
+                id: node.id,
+                label: node.label,
+                // value: node.value,
+                color: color
+              }
             )
           })
 
@@ -27,7 +34,26 @@ export default function PersonNetworkGraph({ name, active_nodes, person_edges })
     nodes: parsed_nodes,
     edges: parsed_edges,
   }
-  const options = {}
+  const options = {
+    interaction: {navigationButtons: true},
+    nodes: {
+      // shape: 'dot',
+      // size: 30,
+      font: {
+        size: 12,
+        color: '#000000',
+      },
+      borderWidth: 2,
+      chosen: true,
+      shape: 'box',
+      margin: 10,
+      opacity: 0.5,
+    },
+    edges: {
+      width: 2,
+      color: {inherit: 'from'},
+    },
+  }
 
   return (
     <>
@@ -35,11 +61,11 @@ export default function PersonNetworkGraph({ name, active_nodes, person_edges })
         <h1>Hello {name}!</h1>
         <h2>Not playin nice with Phlex Layout</h2>
         <p>Try moving the entire People Layout to erb, and see if can drop this JSX rendering into a partial??</p>
-        <p>{active_nodes}</p>
-        <p>{person_edges}</p>
+        {/* <p>{active_nodes}</p>
+        <p>{person_edges}</p> */}
 
 
-        <div>
+        {/* <div>
           {JSON.parse(active_nodes).map((node, index) => {
             return (
               <div>
@@ -48,12 +74,12 @@ export default function PersonNetworkGraph({ name, active_nodes, person_edges })
               </div>
             )
           })}
-        </div>
+        </div> */}
 
         <Graph
           graph={graph}
           options={options}
-          style={{ height: '640px' }}
+          style={{ height: '90vh' }}
         />
       </div>
     </>
