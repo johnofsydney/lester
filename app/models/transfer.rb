d31942b9e2511c4b6b438f1c6c5c3f9c342a70e4
John Coote <john_coote@rocketmail.com>
2025-02-01 15:12:22 +1100

    use a common page nav for group index view

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -13,9 +13,29 @@ class Transfer < ApplicationRecord
     message: "should have unique combination of giver_type, giver_id, taker_id, amount, and effective_date"
   }
 
-  store_accessor :data, %i(donations)
+  def self.financial_years
+    [
+      2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025,
+    ]
+  end
+
+  store_accessor :data, %i(donations giver_name taker_name), prefix: :cached
 
   def financial_year
     effective_date.year
+  end
+
+  def giver_name
+    unless cached_giver_name.present?
+      self.update(cached_giver_name: giver.name)
+    end
+
+    cached_giver_name
+  end
+
+  def taker_name
+    self.update(cached_taker_name: taker.name) unless cached_taker_name.present?
+
+    cached_taker_name
   end
 end
