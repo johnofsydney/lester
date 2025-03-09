3fca73759d089eeb38395c066b05def8f0030814
John Coote <john_coote@rocketmail.com>
2025-03-09 16:08:23 +1100

    JS config for google tag manager

--- app/javascript/controllers/transfer_controller.js
+++ app/javascript/controllers/transfer_controller.js
@@ -1,1 +1,9 @@
 console.log("Hello from transfer_controller.js")
+
+
+console.log("Google Tag Manager is working");
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KRT9EZS95V');
