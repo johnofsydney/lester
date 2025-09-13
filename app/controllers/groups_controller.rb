dbfcf2f0cb0ba933dd896e1853e552fd2f7f920d
John Coote <john.coote@domain.com.au>
2025-09-13 11:31:19 +1000

    do some includes on a query

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -48,7 +48,7 @@ class GroupsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_group
-    @group = Group.find(params[:id])
+    @group = Group.includes(:incoming_transfers, :outgoing_transfers).find_by(id: params[:id])
   end
 
   def set_page
