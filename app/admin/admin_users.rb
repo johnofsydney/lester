5a7b66d3325b4859d2cb94bfe8b68275c1f0f4cb
John Coote <john.coote@domain.com.au>
2025-10-28 20:08:32 +1100

    fix the mennu and the people bug in aa

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -2,13 +2,15 @@ ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
 
   index do
-    selectable_column
-    id_column
-    column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
-    actions
+    if Current.user&.id == 1
+      selectable_column
+      id_column
+      column :email
+      column :current_sign_in_at
+      column :sign_in_count
+      column :created_at
+      actions
+    end
   end
 
   filter :email
