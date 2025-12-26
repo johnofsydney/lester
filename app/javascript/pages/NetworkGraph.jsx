9e6b2a5ac5bf18233ddc56d0b640001324102bad
John Coote <john.coote@domain.com.au>
2025-12-26 12:11:20 +1100

    revert to original colors

--- app/javascript/pages/NetworkGraph.jsx
+++ app/javascript/pages/NetworkGraph.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Graph from 'react-vis-network-graph'
+// https://visjs.github.io/vis-network/docs/network/
+// https://visjs.github.io/vis-network/docs/network/nodes.html
 
 export default function NetworkGraph({ url, name, json_nodes, json_edges }) {
   const nodes = JSON.parse(json_nodes).map((node) => {
