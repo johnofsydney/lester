996fe749b19be3f356eb7c29cf33221df92ff2fd
John Coote <john.coote@domain.com.au>
2026-01-11 13:01:42 +1100

    progressively offset more each time until finished

--- app/sidekiq/backfill_names_from_abn_job.rb
+++ app/sidekiq/backfill_names_from_abn_job.rb
@@ -6,19 +6,23 @@ class BackfillNamesFromAbnJob
   # Can delete when finished
   include Sidekiq::Job
 
-  QUANTITY = 1_000
+  QUANTITY = 2_000
 
-  def perform
+  attr_reader :offset
+
+  def perform(offset = 0)
+    @offset = offset.to_i
+
     groups_to_update.limit(QUANTITY).find_each do |group|
       UpdateGroupNamesFromAbnJob.perform_async(group.id)
     end
 
     # Re-enqueue itself until fully done
-    self.class.perform_in(5.minutes) if more_remaining?
+    self.class.perform_in(4.minutes, offset + QUANTITY) if more_remaining?
   end
 
   def groups_to_update
-    Group.where.not(business_number: nil).order(:id)
+    Group.where.not(business_number: nil).order(:id).offset(offset)
   end
 
   def more_remaining?
