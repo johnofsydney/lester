c7257d47c04b58e20a20add6bb33000be29e423c
John Coote <john.coote@domain.com.au>
2026-01-11 13:19:57 +1100

    rubocop

--- app/services/abn/group_name_updater.rb
+++ app/services/abn/group_name_updater.rb
@@ -18,14 +18,14 @@ class Abn::GroupNameUpdater
     result[:trading_names].each do |trading_name|
       group.trading_names.create(name: trading_name)
     end
-    rescue AbnDetailsSuppressed => e
-      # Many details, including name, are suppressed for this ABN. Don't update the group, don't retry.
-      Rails.logger.info("ABN details suppressed for group: #{group.id} with business number: #{group.business_number}")
-      NewRelic::Agent.notice_error(e, custom_params: { group_id: group.id, business_number: group.business_number })
-    rescue AbnNotFound => e
-      # ABN not found. Don't update the group, don't retry.
-      Rails.logger.info("ABN not found for group: #{group.id} with business number: #{group.business_number}")
-      NewRelic::Agent.notice_error(e, custom_params: { group_id: group.id, business_number: group.business_number })
+  rescue AbnDetailsSuppressed => e
+    # Many details, including name, are suppressed for this ABN. Don't update the group, don't retry.
+    Rails.logger.info("ABN details suppressed for group: #{group.id} with business number: #{group.business_number}")
+    NewRelic::Agent.notice_error(e, custom_params: { group_id: group.id, business_number: group.business_number })
+  rescue AbnNotFound => e
+    # ABN not found. Don't update the group, don't retry.
+    Rails.logger.info("ABN not found for group: #{group.id} with business number: #{group.business_number}")
+    NewRelic::Agent.notice_error(e, custom_params: { group_id: group.id, business_number: group.business_number })
   end
 
   private
