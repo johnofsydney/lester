2e65802cb2a1ae02e10ae44caedc82ac801b964a
John Coote <john.coote@domain.com.au>
2026-02-18 14:33:21 +1100

    tinker with optional

--- db/migrate/20260217030959_create_fine_grained_transaction_categories.rb
+++ db/migrate/20260217030959_create_fine_grained_transaction_categories.rb
@@ -2,7 +2,7 @@ class CreateFineGrainedTransactionCategories < ActiveRecord::Migration[8.0]
   def change
     create_table :fine_grained_transaction_categories do |t|
       t.string :name
-      t.references :major_transaction_category, null: false, foreign_key: true
+      t.references :major_transaction_category, null: true, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20260217031145_add_fine_grained_transaction_category_to_individual_transactions.rb
+++ db/migrate/20260217031145_add_fine_grained_transaction_category_to_individual_transactions.rb
@@ -1,5 +1,5 @@
 class AddFineGrainedTransactionCategoryToIndividualTransactions < ActiveRecord::Migration[8.0]
   def change
-    add_reference :individual_transactions, :fine_grained_transaction_category, foreign_key: true
+    add_reference :individual_transactions, :fine_grained_transaction_category, foreign_key: true, null: true
   end
 end
