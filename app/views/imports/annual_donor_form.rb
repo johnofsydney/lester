a30fc85dbc1209256d1a79044e4f7a2ee23ad23b
John Coote <john.coote@domain.com.au>
2026-02-18 17:34:53 +1100

    write rake task

--- app/views/imports/annual_donor_form.rb
+++ app/views/imports/annual_donor_form.rb
@@ -1,49 +1,49 @@
-class Imports::AnnualDonorForm < Phlex::HTML
-  def view_template
-    h1 { 'Imports' }
+# class Imports::AnnualDonorForm < Phlex::HTML
+#   def view_template
+#     h1 { 'Imports' }
 
-    div do
-      h2 { 'Import AEC Annual Donor Data'}
-      form(action: '/imports/annual_donor_upload', enctype: 'multipart/form-data', method: 'post') do
-        div(class: 'form-group') do
-          input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
-        end
+#     div do
+#       h2 { 'Import AEC Annual Donor Data'}
+#       form(action: '/imports/annual_donor_upload', enctype: 'multipart/form-data', method: 'post') do
+#         div(class: 'form-group') do
+#           input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
+#         end
 
-        button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
-      end
-    end
+#         button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
+#       end
+#     end
 
-    div do
-      h2 { 'Import Cleaned List of Federal Parliamentarians'}
-      form(action: '/imports/federal_parliamentarians_upload', enctype: 'multipart/form-data', method: 'post') do
-        div(class: 'form-group') do
-          input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
-        end
+#     div do
+#       h2 { 'Import Cleaned List of Federal Parliamentarians'}
+#       form(action: '/imports/federal_parliamentarians_upload', enctype: 'multipart/form-data', method: 'post') do
+#         div(class: 'form-group') do
+#           input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
+#         end
 
-        button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
-      end
-    end
+#         button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
+#       end
+#     end
 
-    div do
-      h2 { 'Import Peoples Names'}
-      form(action: '/imports/people_upload', enctype: 'multipart/form-data', method: 'post') do
-        div(class: 'form-group') do
-          input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
-        end
+#     div do
+#       h2 { 'Import Peoples Names'}
+#       form(action: '/imports/people_upload', enctype: 'multipart/form-data', method: 'post') do
+#         div(class: 'form-group') do
+#           input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
+#         end
 
-        button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
-      end
-    end
+#         button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
+#       end
+#     end
 
-    div do
-      h2 { 'Import Groups Names'}
-      form(action: '/imports/groups_upload', enctype: 'multipart/form-data', method: 'post') do
-        div(class: 'form-group') do
-          input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
-        end
+#     div do
+#       h2 { 'Import Groups Names'}
+#       form(action: '/imports/groups_upload', enctype: 'multipart/form-data', method: 'post') do
+#         div(class: 'form-group') do
+#           input(class: 'form-control', type: 'file', value: 'filename', name: 'project[filename]') { '' }
+#         end
 
-        button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
-      end
-    end
-  end
-end
+#         button(class: 'btn btn-primary', type: 'submit') { 'Submit' }
+#       end
+#     end
+#   end
+# end
