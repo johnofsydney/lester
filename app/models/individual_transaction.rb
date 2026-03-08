5619372e012f911dcc9546ec0cc0c3297264f303
John Coote <john.coote@domain.com.au>
2026-03-08 18:46:38 +1100

    toughen up individual transactions model, remove job that has been run

--- app/models/individual_transaction.rb
+++ app/models/individual_transaction.rb
@@ -1,8 +1,8 @@
 class IndividualTransaction < ApplicationRecord
   belongs_to :transfer
-  belongs_to :giver, polymorphic: true, optional: true # remove optional later
-  belongs_to :taker, polymorphic: true, optional: true # remove optional later
-  belongs_to :fine_grained_transaction_category, optional: true # remove optional later
+  belongs_to :giver, polymorphic: true
+  belongs_to :taker, polymorphic: true
+  belongs_to :fine_grained_transaction_category
   has_one :major_transaction_category, through: :fine_grained_transaction_category
 
   validates :amount, presence: true
