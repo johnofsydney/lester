b3f4fb2bad53ca25376acceaf097fd9f09e09010
John Coote <john.coote@domain.com.au>
2026-03-12 09:08:03 +1100

    tests and rubocop

--- app/services/nodes/name_capitalizer.rb
+++ app/services/nodes/name_capitalizer.rb
@@ -5,8 +5,8 @@ class Nodes::NameCapitalizer
     exceptions = %w[The Pty Ltd Inc Co Van Aus Of And For As Is Hire Bank Home Host Plus Greg Paul Sole John Pete Jan Troy Lucy Sole Tax Aid Job Marc Sep Mark Kate Sam No East West Care Mind Menu Sub Fund]
 
     CapitalizeNames.capitalize(name)
-                   .gsub(/(\()([a-z])/) { "#{$1}#{$2.upcase}" }
-                   .gsub(/(\d)([a-z])/) { "#{$1}#{$2.upcase}" }
+                   .gsub(/(\()([a-z])/) { "#{::Regexp.last_match(1)}#{::Regexp.last_match(2).upcase}" }
+                   .gsub(/(\d)([a-z])/) { "#{::Regexp.last_match(1)}#{::Regexp.last_match(2).upcase}" }
                    .gsub(/\b\w{2,4}\b/) do |acronym|
                      exceptions.include?(acronym) ? acronym : acronym.upcase
                    end
