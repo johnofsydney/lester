5d5249dadcd180c78e46c51d76bfc9f6b1cd8d49
John Coote <john.coote@domain.com.au>
2026-03-12 17:59:39 +1100

    further implement the name capitalizer

--- app/services/nodes/name_capitalizer.rb
+++ app/services/nodes/name_capitalizer.rb
@@ -1,16 +1,16 @@
 require 'capitalize_names'
 
 class Nodes::NameCapitalizer
-  PEOPLE_NAMES = %w[Greg Paul John Pete Jan Troy Lucy Marc Sep Mark Kate Sam]
-  BUSINESS_WORDS = %w[Pty Ltd Inc Co Aus Hire Bank Host Plus Sole Tax Aid Job Sub Fund]
-  OTHER_WORDS = %w[The Van Of And For As Is Home No East West Mind Menu Sub Fund]
+  PEOPLE_NAMES = %w[Greg Paul John Pete Jan Troy Lucy Marc Sep Mark Kate Sam].freeze
+  BUSINESS_WORDS = %w[Pty Ltd Inc Co Aus Hire Bank Host Plus Sole Tax Aid Job Sub Fund Toll Root Web Data Corp Hays].freeze
+  OTHER_WORDS = %w[The Van Of And For As Is Home No East West Mind Menu Sub Fund Let Talk Red Man Hat Asia Hive Ping].freeze
 
   def self.capitalize(name)
     exceptions = PEOPLE_NAMES + BUSINESS_WORDS + OTHER_WORDS
 
     CapitalizeNames.capitalize(name)
-                   .gsub(/(\()([a-z])/) { "#{::Regexp.last_match(1)}#{::Regexp.last_match(2).upcase}" }
-                   .gsub(/(\d)([a-z])/) { "#{::Regexp.last_match(1)}#{::Regexp.last_match(2).upcase}" }
+                   .gsub(/(\()([a-z])/) { "#{::Regexp.last_match(1)}#{::Regexp.last_match(2).upcase}" } # capitalise first letter after an open bracket
+                   .gsub(/(\d)([a-z])/) { "#{::Regexp.last_match(1)}#{::Regexp.last_match(2).upcase}" } # capitalise first letter after a number
                    .gsub(/\b\w{2,4}\b/) do |acronym|
                      exceptions.include?(acronym) ? acronym : acronym.upcase
                    end
