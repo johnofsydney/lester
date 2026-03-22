5cff5465fc0a1fc4f233dc8986bb2cc39986eaed
John Coote <john.coote@domain.com.au>
2026-03-22 18:08:45 +1100

    correct a typo

--- app/sidekiq/backfill_aec_id_to_external_identifiers.rb
+++ app/sidekiq/backfill_aec_id_to_external_identifiers.rb
@@ -1,9 +1,9 @@
-class BackfillAecIdToExternalIdentifers
-  # Migrates existing aec_id columns on people and groups into the external_identifers table.
-  # Run after deploying the CreateExternalIdentifers migration.
+class BackfillAecIdToExternalIdentifiers
+  # Migrates existing aec_id columns on people and groups into the external_identifiers table.
+  # Run after deploying the CreateExternalIdentifiers migration.
   #
   # Trigger from console with:
-  #   BackfillAecIdToExternalIdentifers.perform_async
+  #   BackfillAecIdToExternalIdentifiers.perform_async
   include Sidekiq::Job
 
   BATCH_SIZE = 500
@@ -18,7 +18,7 @@ class BackfillAecIdToExternalIdentifers
   def backfill(klass)
     klass.where.not(aec_id_legacy: [nil, '']).find_in_batches(batch_size: BATCH_SIZE) do |batch|
       batch.each do |record|
-        ExternalIdentifer.find_or_create_by!(
+        ExternalIdentifier.find_or_create_by!(
           owner_type: klass.name,
           owner_id: record.id,
           source: 'aec',
