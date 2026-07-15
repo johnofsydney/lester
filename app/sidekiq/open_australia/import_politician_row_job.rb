57b2b9cea6c2364fc0829ed49329f38947e32091
John Coote <john_coote@rocketmail.com>
2026-07-15 20:23:02 +1000

    add a sleep

--- app/sidekiq/open_australia/import_politician_row_job.rb
+++ app/sidekiq/open_australia/import_politician_row_job.rb
@@ -4,10 +4,12 @@ class OpenAustralia::ImportPoliticianRowJob
   sidekiq_options(
     lock: :until_executed,
     on_conflict: :log,
-    retry: 1
+    retry: 1,
+    queue: 'low'
   )
 
   def perform(person_id, house)
+    sleep 0.3
     OpenAustralia::ImportPoliticianRow.call(person_id:, house:)
   end
 end
