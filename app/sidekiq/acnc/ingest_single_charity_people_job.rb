ebf1e92493d2382ba6f1f5e5266498ab21ed165f
John Coote <john_coote@rocketmail.com>
2026-08-22 09:24:57 +1000

    Fix Lint/DuplicateBranch in Acnc::IngestSingleCharityPeopleJob
    Merge rescue clauses with identical bodies into two groups: not-found
    errors (no retry) and anticipated errors (retry).

--- app/sidekiq/acnc/ingest_single_charity_people_job.rb
+++ app/sidekiq/acnc/ingest_single_charity_people_job.rb
@@ -11,22 +11,12 @@ class Acnc::IngestSingleCharityPeopleJob
   def perform(charity_id)
     charity = Group.find(charity_id)
     AcncCharities::FetchSingleCharityPeople.call(charity)
-  rescue ActiveRecord::RecordNotFound => e
+  rescue ActiveRecord::RecordNotFound, NoResultsFound => e
     Rails.logger.error "Charity not found for Acnc::IngestSingleCharityPeopleJob: #{charity_id}"
     ApiLog.create(message: e.message)
     # Don't re-raise - this won't be fixed by retrying
-  rescue ResponseFailed, NoResultsFound => e
+  rescue ResponseFailed, Net::ReadTimeout => e
     # anticipated errors
-    Rails.logger.error "Error processing Acnc::IngestSingleCharityPeopleJob: #{e.message} - will retry"
-    Rails.logger.error e.backtrace.join("\n")
-    ApiLog.create(message: e.message)
-    raise e
-  rescue Net::ReadTimeout => e
-    Rails.logger.error "Error processing Acnc::IngestSingleCharityPeopleJob: #{e.message} - will retry"
-    Rails.logger.error e.backtrace.join("\n")
-    ApiLog.create(message: e.message)
-    raise e
-  rescue StandardError => e
     Rails.logger.error "Error processing Acnc::IngestSingleCharityPeopleJob: #{e.message} - will retry"
     Rails.logger.error e.backtrace.join("\n")
     ApiLog.create(message: e.message)
